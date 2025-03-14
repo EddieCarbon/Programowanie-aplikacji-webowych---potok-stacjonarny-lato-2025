818d815f6d6634a0ea9ea25d95a4abdf11468523
Jakub Tomiczek <tomiczekkuba01@gmail.com>
2025-03-14 17:54:24 +0100

    Init

---
+++ lab 1/managme/src/services/projectService.ts
@@ -0,0 +1,42 @@
+export interface Project {
+  id: string;
+  name: string;
+  description: string;
+}
+
+export class ProjectService {
+  private static readonly STORAGE_KEY = "projects";
+
+  static getAllProjects(): Project[] {
+    const projects = localStorage.getItem(this.STORAGE_KEY);
+    return projects ? JSON.parse(projects) : [];
+  }
+
+  static getProjectById(id: string): Project | undefined {
+    const projects = this.getAllProjects();
+    return projects.find((project) => project.id === id);
+  }
+
+  static createProject(project: Project): void {
+    const projects = this.getAllProjects();
+    projects.push(project);
+    localStorage.setItem(this.STORAGE_KEY, JSON.stringify(projects));
+  }
+
+  static updateProject(updatedProject: Project): void {
+    const projects = this.getAllProjects();
+    const projectIndex = projects.findIndex(
+      (project) => project.id === updatedProject.id,
+    );
+    if (projectIndex !== -1) {
+      projects[projectIndex] = updatedProject;
+      localStorage.setItem(this.STORAGE_KEY, JSON.stringify(projects));
+    }
+  }
+
+  static deleteProject(id: string): void {
+    const projects = this.getAllProjects();
+    const updatedProjects = projects.filter((project) => project.id !== id);
+    localStorage.setItem(this.STORAGE_KEY, JSON.stringify(updatedProjects));
+  }
+}
